513a21023b55b3366b245e23b2e847d459335685
memisju <125404782+lopeslima@users.noreply.github.com>
2023-03-01 17:50:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@
 function bot(){
 var usuario=document.getElementById("brunetti").value;
 var camp="voce"
-document.getElementById("aplicativo.sorteio").innerHTML;
+document.getElementById("resposta").innerHTML;
